f54e36d6a3cd31149506973c6b5a0cf9b660410c
Hannes Preishuber <hannesp@ppedv.de>
2022-09-30 09:47:18 +0200

    Person Model mit Anotationen

---
+++ BlazorTrainingSept22/Pages/Modul5/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTrainingSept22.Pages.Modul5
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Da muss was rein")]
+        [StringLength(10,ErrorMessage ="max 10")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Da muss was rein")]
+        [Range(140,190,ErrorMessage ="zwischen 140 und 190")]
+        public int Gross { get; set; }
+        public DateTime GebDat { get; set; }
+    }
+}
