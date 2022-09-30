247aeed6bd142c01a3b29ff24c839e169c8bf338
Hannes Preishuber <hannesp@ppedv.de>
2022-09-30 09:57:43 +0200

    HTML INput Grundlagen

---
+++ BlazorTrainingSept22/Pages/Modul5/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTrainingSept22.Pages.Modul5
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
